0a0878ed7d034670fea4099629fe5fa3a917f16f
mardy-codev <mardy.codev@gmail.com>
2018-07-05 17:57:24 +0800

    Commit base

---
+++ resources/assets/js/components/MoreNewComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">More New Component</div>
+
+                    <div class="card-body">
+                        <h1 v-on:click="greet">{{ content }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                content: 'Hello World'
+            }
+        },
+        mounted() {
+            console.log('Yummy mounted.')
+        },
+       methods: {
+            greet: function (event) {
+                // `this` inside methods points to the Vue instance
+                alert('Hello Mardy!')
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/NewComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">New Component</div>
+
+                    <div class="card-body">
+                        I'm a new component. <br>
+                        This is how we play. <br>
+                        This is a hellow world program. <br>
+                        Halller! hahahahahahaha
+                        <div>Why like that or why like this???</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Yummy mounted.')
+        }
+    }
+</script>
